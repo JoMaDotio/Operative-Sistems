e6e9976543f3253b4fee9e6bbe881e58dc3d2f0f
JoMaDotio <eltaquero@hotmail.com>
2022-03-01 23:53:01 -0600

    I think that practice 3 its complete, needs to test a lot

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from asyncore import read
 import msvcrt
 from operator import le
 import os
@@ -24,7 +25,7 @@ def valid_operation(num1, ope, num2):
     return True
 
 
-def capture_process(value, to, cont_p, num_l):
+def capture_process(value, to, cont_p):
     operators = ["+", "-", "*", "/", "%"]
     id_control = 1
     while(value > 0):
@@ -51,20 +52,20 @@ def capture_process(value, to, cont_p, num_l):
             if (valid_operation(num1, operador, num2)):
                 break
             continue
-        if cont_p % 5 == 0:
-            num_l += 1
         cont_p += 1
         value -= 1
         tmp = Proceso(tms, id, operador, num1, num2)
-        tmp.num_lote = num_l
         tmp.do_operation()
         to.append(tmp)
         os.system("cls")
         id_control += 1
-    return num_l
+    return
 
 
-def handle_keyboard_event(flag):
+def handle_keyboard_event(flag, flag_endless):
+
+    if flag_endless:
+        return flag
     if msvcrt.kbhit():
         leter = msvcrt.getch().decode("ASCII")
 
@@ -85,73 +86,137 @@ def handle_keyboard_event(flag):
     return flag
 
 
+def update_bloked(block, ready_arg):
+    if len(block) == 0:
+        return
+    for i in range(len(block)):
+        block[i].blocked_time += 1
+        if block[i].blocked_time == 8:
+            block[i].blocked_time = 0
+            ready_arg.append(block.pop(i))
+            return True
+    return False
+
+
 def main():
-    to_do = []
-    in_execution = None
+
+    endless_process = Proceso(100, "Proceso nulo", "+", 1, 1)
+    endless_flag = False
+
+    news = []
     done = []
-    pack = []
+    ready = []
+    bloked = []
+    in_execution = []
     global_counter = 0
-    num_lotes = 0
     cont_process = 0
     num_procesos = int(input("Cuantos procesos quieres: "))
-    num_lotes = capture_process(num_procesos, to_do, cont_process, num_lotes)
+    capture_process(num_procesos, news, cont_process)
     flag = "c"
-    while(len(to_do) > 0):
+    flag_done = False
+    update = False
+    cont_ready = len(ready)
+    cont_blocked = len(bloked)
+    cont_inexecution = len(in_execution)
+    on_memory = cont_ready + cont_inexecution + cont_blocked
+    while(not flag_done):
+        cont_ready = len(ready)
+        cont_blocked = len(bloked)
+        cont_inexecution = len(in_execution)
+        on_memory = cont_ready + cont_inexecution + cont_blocked
 
-        how_many_left = len(to_do)
-        if (how_many_left > 5):
-            for i in range(5):
-                pack.append(to_do.pop(0))
+        while (on_memory < 5 and len(news) > 0):
+            temp_update = news.pop(0)
+            temp_update.come_time = global_counter
+            print("----Aqui todo bien----", temp_update.come_time)
+            ready.append(temp_update)
+            on_memory += 1
+            cont_ready += 1
+
+        if on_memory > 0 and cont_ready != 0:
+            if endless_flag:
+                in_execution.pop()
+                endless_flag = False
+            tmp = ready.pop(0)
+            tmp.response_time = global_counter - tmp.come_time
+            in_execution.append(tmp)
         else:
-            for i in range(how_many_left):
-                pack.append(to_do.pop(0))
-        num_lotes -= 1
+            in_execution.append(endless_process)
+            endless_flag = True
+        time_left = in_execution[0].time
+        time_on = 0
+        if (in_execution[0].time_remainder != None):
+            time_left = in_execution[0].time_remainder
+            time_on = in_execution[0].time - in_execution[0].time_remainder
+        while (time_on < time_left):
+            if endless_flag and len(news) < 1 and on_memory < 1:
+                endless_flag = False
+                break
+            if endless_flag and update:
+                break
+            flag = handle_keyboard_event(flag, endless_flag)
+            if flag == "p":
+                continue
+            elif flag == "e":
+                in_execution[0].result = "Error"
+                in_execution[0].end_time = global_counter
+                in_execution[0].execute_time = in_execution[0].time - time_on
+                break
+            elif flag == "i":
+                in_execution[0].time_remainder = in_execution[0].time - time_on
+                bloked.append(in_execution[0])
+                in_execution.pop()
+                break
+            print(time_left, time_on)
+            print("Contador global: " + str(global_counter))
+            time_on += 1
+            global_counter += 1
+            update = update_bloked(bloked, ready)
+            print("------------------procesos en listo----------")
+            print("Id\tTMS\tTR")
+            for p in ready:
+                p.show_next()
+            print("\n-------proceso en ejecucion--------")
+            in_execution[0].show_info()
+            print("Tiempo restante: " + str(time_left))
+            print("Tiempo avanzado: " + str(time_on))
+            print("\n----------Procesos bloquedos-----------")
+            print("Id\tTR\tTB")
+            for p in bloked:
+                p.show_bloked()
+            print("\n----------Procesos terminados----------")
+            print("Id\tOperacion\tResultado")
+            for pro in done:
+                pro.show_end()
+            sleep(0.1)
+            if flag != "p":
+                os.system("cls")
 
-        while(len(pack) > 0):
-            in_execution = pack.pop(0)
-            time_left = in_execution.time
-            time_on = 0
-            if (in_execution.time_remainder != None):
-                time_left = in_execution.time_remainder
-                time_on = in_execution.time - in_execution.time_remainder
-            while(time_on != in_execution.time):
-                flag = handle_keyboard_event(flag)
-                if flag == "p":
-                    continue
-                elif flag == "e":
-                    in_execution.result = "Error"
-                    break
-                elif flag == "i":
-                    in_execution.time_remainder = in_execution.time - time_on
-                    pack.append(in_execution)
-                    break
-                print("Lotes restantes: " + str(num_lotes))
-                print("Contador global: " + str(global_counter))
-                time_left -= 1
-                time_on += 1
-                global_counter += 1
-                print("------------------procesos del lote----------")
-                print("Id\tTMS")
-                for p in pack:
-                    p.show_next()
-                print("\n-------proceso en ejecucion--------")
-                in_execution.show_info()
-                print("Tiempo restante: " + str(time_left))
-                print("Tiempo avanzado: " + str(time_on))
-                print("\n----------Procesos terminados----------")
-                print("Id\tOperacion\tResultado\tNum Lote")
-                for pro in done:
-                    pro.show_end()
-                sleep(1)
-                if flag != "p":
-                    os.system("cls")
-            if flag != "i":
-                done.append(in_execution)
-            flag = "c"
+        if flag != "i" and not endless_flag:
+            temp_t = in_execution[0]
+            if in_execution[0].end_time == None:
+                in_execution[0].end_time = global_counter
+            if in_execution[0].execute_time == None:
+                in_execution[0].execute_time = in_execution[0].time
+            aux1 = temp_t.end_time
+            aux3 = temp_t.come_time
+            print("Bug: ", in_execution[0].come_time)
+            #in_execution[0].return_time = aux1 - aux3
+            aux2 = in_execution[0].return_time
+            #in_execution[0].wait_time = aux2-in_execution[0].execute_time
+            done.append(in_execution.pop(0))
+        flag = "c"
+        if endless_flag:
+            continue
+        if (len(news) == 0 and on_memory == 0):
+            flag_done = True
+
     print("Contador global: " + str(global_counter))
-    print("Id\tOperacion\tResultado\tNum Lote")
+    print("Id\tOperacion\tResultado\tTLL\tTS\tTR\tTRES\tTES\tTE")
     for pro in done:
-        pro.show_end()
+        if pro.id == "Proceso nulo":
+            continue
+        pro.show_end_program()
 
 
 main()
--- proceso.py
+++ proceso.py
@@ -1,14 +1,24 @@
-# hello
 class Proceso:
     def __init__(self, time, id, ope, num1, num2):
         self.id = id
-        self.time = time
-        self.time_remainder = None
-        self.result = None
         self.ope = ope
         self.num1 = num1
         self.num2 = num2
-        self.num_lote = None
+        self.result = None
+        self.time = time
+        self.time_remainder = None
+        self.come_time = None
+        self.end_time = None
+        self.return_time = None
+        self.execute_time = None
+        self.response_time = None
+        self.wait_time = None
+        self.blocked_time = 0
+
+    def calculate_times(self):
+        self.return_time = self.end_time - self.come_time
+        self.response_time = self.execute_time - self.come_time
+        self.wait_time = self.return_time - (self.time - self.time_remainder)
 
     def show_info(self):
         print("Id: " + str(self.id))
@@ -16,11 +26,22 @@ class Proceso:
         print("TME: " + str(self.time))
 
     def show_next(self):
-        print(f"{self.id}\t{self.time}")
+        print(f"{self.id}\t{self.time}\t{self.time_remainder}")
 
     def show_end(self):
+        if self.result == "Error":
+            rest = "Error"
+        else:
+            rest = f"{self.result:.2f}"
         print(
-            f"{self.id}\t{str(self.num1) + self.ope + str(self.num2)}\t\t{self.result}\t\t{self.num_lote}")
+            f"{self.id}\t{str(self.num1) + self.ope + str(self.num2)}\t\t{rest}")
+
+    def show_end_program(self):
+        if self.result == "Error":
+            rest = "Error"
+        else:
+            rest = f"{self.result:.2f}"
+        print(f"{self.id}\t{str(self.num1) + self.ope + str(self.num2)}\t\t\{rest}\t\t{self.come_time}\t{self.end_time}\t{self.end_time-self.come_time}\t{self.response_time}\t{(self.end_time-self.come_time)-self.execute_time}\t{self.execute_time}")
 
     def do_operation(self):
         if self.ope == "/" or self.ope == "%":
@@ -41,3 +62,6 @@ class Proceso:
             return False
 
         return True
+
+    def show_bloked(self):
+        print(f"{self.id}\t{self.time-self.time_remainder}\t{self.blocked_time}")
